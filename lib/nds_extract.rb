16150ef61dce9aff18b4a3d444e4e5a2e5338849
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-30 20:26:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,4 +24,13 @@ def directors_totals(nds)
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  
+  gross = 0
+  count = 0
+
+  while count < director_data[:movies].count do
+    gross += director_data[:movies][count][:worldwide_gross]
+    count += 1
+  end
+
+  gross
+end
