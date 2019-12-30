84a5b8eb96142877b32e71360c8b2d7a4e79111f
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-30 20:18:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,21 +9,8 @@ require 'directors_database'
 def directors_totals(nds)
   # result = {}
   # nil
-   pp nds
-directors_totals = {} 
- row_index = 0 
- while row_index < nds.length do
-  directors_name = nds[row_index][:name] 
-  directors_totals[directors_name] = 0 
-  movie_index = 0
-  while movie_index < nds[row_index][:movies].length do
-  directors_totals[directors_name] += nds[row_index][:movies][movie_index][:worldwide_gross]
-  movie_index += 1
-  end
-  row_index += 1
-end
-directors_totals
-end
+   
+ 
 
 
 
